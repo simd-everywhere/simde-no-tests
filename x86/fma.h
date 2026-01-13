simd-everywhere/simde

x86 fma: improve fallback for simde_mm256_fmadd_pd

simde/x86/fma.h
@@ -82,6 +82,16 @@ simde_mm256_fmadd_pd (simde__m256d a, simde__m256d b, simde__m256d c) {
     return _mm256_fmadd_pd(a, b, c);
   #elif defined(SIMDE_LOONGARCH_LASX_NATIVE)
     return __lasx_xvfmadd_d(a, b, c);
+  #elif SIMDE_NATURAL_VECTOR_SIZE_LE(128)
+   simde__m256d_private
+     r_,
+     a_ = simde__m256d_to_private(a),
+     b_ = simde__m256d_to_private(b),
+     c_ = simde__m256d_to_private(c);
+
+   r_.m128d[0] = _mm_fmadd_pd(a_.m128d[0], b_.m128d[0], c_.m128d[0]);
+   r_.m128d[1] = _mm_fmadd_pd(a_.m128d[1], b_.m128d[1], c_.m128d[1]);
+   return simde__m256d_from_private(r_);
   #else
     return simde_mm256_add_pd(simde_mm256_mul_pd(a, b), c);
   #endif

simd-everywhere/simde-no-tests

Co-authored-by: Victor Shoup <victor@shoup.net>

x86/fma.h
@@ -82,6 +82,16 @@ simde_mm256_fmadd_pd (simde__m256d a, simde__m256d b, simde__m256d c) {
     return _mm256_fmadd_pd(a, b, c);
   #elif defined(SIMDE_LOONGARCH_LASX_NATIVE)
     return __lasx_xvfmadd_d(a, b, c);
+  #elif SIMDE_NATURAL_VECTOR_SIZE_LE(128)
+   simde__m256d_private
+     r_,
+     a_ = simde__m256d_to_private(a),
+     b_ = simde__m256d_to_private(b),
+     c_ = simde__m256d_to_private(c);
+
+   r_.m128d[0] = simde_mm_fmadd_pd(a_.m128d[0], b_.m128d[0], c_.m128d[0]);
+   r_.m128d[1] = simde_mm_fmadd_pd(a_.m128d[1], b_.m128d[1], c_.m128d[1]);
+   return simde__m256d_from_private(r_);
   #else
     return simde_mm256_add_pd(simde_mm256_mul_pd(a, b), c);
   #endif